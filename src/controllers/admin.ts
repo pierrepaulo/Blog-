3b4688fb9f209aeea29a9b7e61ae6074e4bae316
Pierre Paulo <pierresantista@hotmail.com>
2025-09-30 16:16:54 -0300

    feat(posts): criar endpoint para enviar novo post

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -1,3 +1,57 @@
-import { RequestHandler } from "express";
+import { RequestHandler, Response } from "express";
+import { ExtendedRequest } from "../types/extended-request";
+import z from "zod";
+import { createPost, createPostSlug, handleCover } from "../services/post";
+import { getUserById } from "../services/user";
+import { coverToUrl } from "../utils/cover-to-url";
 
-export const addPost: RequestHandler = async (req, res) => {};
+export const addPost = async (req: ExtendedRequest, res: Response) => {
+  if (!req.user) return;
+
+  const schema = z.object({
+    title: z.string(),
+    tags: z.string(),
+    body: z.string(),
+  });
+  const data = schema.safeParse(req.body);
+  if (!data.success) {
+    res.json({ error: data.error.flatten().fieldErrors });
+    return;
+  }
+
+  if (!req.file) {
+    res.json({ error: "Sem Arquivo" });
+    return;
+  }
+
+  const coverName = await handleCover(req.file);
+  if (!coverName) {
+    res.json({ error: "Imagem nao permitida/enviada" });
+    return;
+  }
+
+  const slug = await createPostSlug(data.data.title);
+
+  const newPost = await createPost({
+    authorId: req.user.id,
+    slug,
+    title: data.data.title,
+    tags: data.data.tags,
+    body: data.data.body,
+    cover: coverName,
+  });
+
+  const author = await getUserById(newPost.authorId);
+
+  res.status(201).json({
+    post: {
+      id: newPost.id,
+      slug: newPost.slug,
+      title: newPost.title,
+      createdAt: newPost.createdAt,
+      cover: coverToUrl(newPost.cover),
+      tags: newPost.tags,
+      authorName: author?.name,
+    },
+  });
+};
---
+++ src/services/post.ts
@@ -0,0 +1,60 @@
+import { v4 } from "uuid";
+import fs from "fs/promises";
+import slug from "slug";
+import { prisma } from "../libs/prisma";
+
+export const getPostBySlug = async (slug: string) => {
+  return await prisma.post.findUnique({
+    where: { slug },
+    include: {
+      author: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+};
+
+export const handleCover = async (file: Express.Multer.File) => {
+  const allowed = ["image/jpeg", "image/jpg", "image/png"];
+  if (allowed.includes(file.mimetype)) {
+    const coverName = `${v4()}.jpg`;
+    try {
+      await fs.rename(file.path, `./public/images/covers/${coverName}`);
+    } catch (err) {
+      return false;
+    }
+    return coverName;
+  }
+  return false;
+};
+
+export const createPostSlug = async (title: string) => {
+  let newSlug = slug(title);
+  let keepTrying = true;
+  let postCount = 1;
+
+  while (keepTrying) {
+    const post = await getPostBySlug(newSlug);
+    if (!post) {
+      keepTrying = false;
+    } else {
+      newSlug = slug(`${title} ${++postCount}`);
+    }
+  }
+
+  return newSlug;
+};
+
+type CreatePostProps = {
+  authorId: number;
+  slug: string;
+  title: string;
+  tags: string;
+  body: string;
+  cover: string;
+};
+export const createPost = async (data: CreatePostProps) => {
+  return await prisma.post.create({ data });
+};
