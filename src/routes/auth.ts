394f69fcd6f68ef8e1308c5e7320b86d12088a9b
Pierre Paulo <pierresantista@hotmail.com>
2025-09-30 14:08:31 -0300

    feat(auth) logando com usuario

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,5 +4,5 @@ import * as authController from "../controllers/auth";
 export const authRoutes = Router();
 
 authRoutes.post("/signup", authController.signup);
-//authRoutes.post("/signin", authController.signin);
+authRoutes.post("/signin", authController.signin);
 //authRoutes.post("/validate", authController.validate);
--- src/services/user.ts
+++ src/services/user.ts
@@ -25,3 +25,17 @@ export const createUser = async ({
     data: { name, email, password: newPassword },
   });
 };
+
+type VerifyUserProps = {
+  email: string;
+  password: string;
+};
+export const verifyUser = async ({ email, password }: VerifyUserProps) => {
+  const user = await prisma.user.findFirst({
+    where: { email },
+  });
+  if (!user) return false;
+  if (!bcrypt.compareSync(password, user.password)) return false;
+
+  return user;
+};
